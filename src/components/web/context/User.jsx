1ee3b735ec26dd7f3dab86b5aeb4085a7389b127
Mohammad Adhami <mohammad.adhami@muc.edu.ps>
2024-03-24 15:19:31 +0200

    add .env

--- src/components/web/context/User.jsx
+++ src/components/web/context/User.jsx
@@ -7,7 +7,7 @@ export function UserContextProvider({children}){
     let [userData,setUserData] = useState(null);
     let [loading,setLoading] = useState(true);
     const getUserData = async()=>{
-        const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/user/profile`,{headers:{Authorization:`__${userToken}`}});
+        const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/user/profile`,{headers:{Authorization:`Tariq__${userToken}`}});
         setUserData(data.user);
         setLoading(false);
 
